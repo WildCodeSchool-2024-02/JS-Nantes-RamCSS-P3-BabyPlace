e7361f136e7f246b2d357d0840cfabe13ef5fa77
Pas-nou <pascal.renou@live.fr>
2024-06-10 17:15:52 +0200

    USY90 - Add Pro Navbar

---
+++ client/src/pages/pages_pro-inscription/InscriptionProEcran1.jsx
@@ -0,0 +1,11 @@
+import NavbarInscriptionPro from "../../components/components_pro-inscription/NavbarInscriptionPro";
+
+function InscriptionProEcran1() {
+  return (
+    <div>
+      <NavbarInscriptionPro />
+    </div>
+  );
+}
+
+export default InscriptionProEcran1;
