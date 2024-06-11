26f37d887134f899c8ceb0462ae64b61d2ddc486
Pas-nou <pascal.renou@live.fr>
2024-06-11 17:37:06 +0200

    Add screen 1 Structure for professional inscription

---
+++ client/src/pages/pages_pro-inscription/InscriptionPro.jsx
@@ -0,0 +1,15 @@
+import NavbarInscriptionPro from "../../components/components_pro-inscription/NavbarInscriptionPro";
+import StructureComponent from "../../components/components_pro-inscription/StructureComponent";
+
+import "../styles_pro_pages/InscriptionPro.css";
+
+function InscriptionPro() {
+  return (
+    <div>
+      <NavbarInscriptionPro />
+      <StructureComponent />
+    </div>
+  );
+}
+
+export default InscriptionPro;
