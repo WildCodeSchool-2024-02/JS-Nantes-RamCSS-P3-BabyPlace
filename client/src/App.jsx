44ba67dec7f3989d0cce03b704a462b3cd14cdc3
Adeline Cornier <adeline.cornier@hotmail.com>
2024-06-13 14:54:45 +0200

    USY-1_Accueil / Page Accueil Parent resolve conflits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import AccueilParents from "./pages/page_parents/AccueilParents";
+import InscriptionPro from "./pages/pages_pro-inscription/InscriptionPro";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <AccueilParents />
+      <InscriptionPro />
     </div>
   );
 }
