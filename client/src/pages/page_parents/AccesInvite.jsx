935cddaf4816dc287648f019de640ca95a6bc85d
Adeline Cornier <adeline.cornier@hotmail.com>
2024-06-19 13:55:27 +0200

    USY-4_Accueil css mise en place

--- client/src/pages/page_parents/AccesInvite.jsx
+++ client/src/pages/page_parents/AccesInvite.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import "../styles_parents/AccesInvite.css";
 
@@ -9,31 +10,55 @@ function AccesInvite() {
   };
 
   return (
-    <div className="form-acces-invite">
-      <img
-        src="./src/assets/images/logos/logo_complet.png"
-        className="logo-invite"
-        alt="logo babyplace"
-      />
-      <h2 className="titles titles-acces-invite">
-        {" "}
-        Garde d'enfant à la demande{" "}
-      </h2>
-      <div className="form-invite">
-        <input type="text" id="name" placeholder="Adresse" />
-        <input type="text" id="name" placeholder="Occasionnel" />
-        <input type="text" id="name" placeholder="Date et heure de début" />
-        <input type="text" id="name" placeholder="Date et heure de fin" />
-        <div className="accesinvite-checkbox-container">
-          <label htmlFor="flexibilité-dates-horaires">
-            <input type="checkbox" onChange={coche} />
-            Mes dates ou mes horaires sont flexibles
-          </label>
-        </div>
-        <button type="submit" className="button-search">
+    <div>
+      <div className="form-acces-invite">
+        <img
+          src="./src/assets/images/logos/logo_complet.png"
+          className="logo-invite"
+          alt="logo babyplace"
+        />
+        <h2 className="titles titles-acces-invite">
           {" "}
-          Rechercher{" "}
-        </button>
+          Garde d'enfant à la demande{" "}
+        </h2>
+        <div className="form-invite">
+          <input
+            className="input-parents-invite"
+            type="text"
+            id="name"
+            placeholder="Adresse"
+          />
+          <input
+            className="input-parents-invite"
+            type="text"
+            id="name"
+            placeholder="Occasionnel"
+          />
+          <input
+            className="input-parents-invite"
+            type="text"
+            id="name"
+            placeholder="Date et heure de début"
+          />
+          <input
+            className="input-parents-invite"
+            type="text"
+            id="name"
+            placeholder="Date et heure de fin"
+          />
+          <div className="accesinvite-checkbox-container">
+            <label htmlFor="flexibilité-dates-horaires">
+              <input type="checkbox" onChange={coche} />
+              Mes dates ou mes horaires sont flexibles
+            </label>
+          </div>
+          <NavLink
+            to="/recherche"
+            className="link-connexion-parent-btn btn-global texts"
+          >
+            Rechercher
+          </NavLink>
+        </div>
       </div>
     </div>
   );
