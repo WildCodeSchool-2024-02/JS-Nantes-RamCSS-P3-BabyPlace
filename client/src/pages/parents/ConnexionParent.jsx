c2a443c5a9d0fcba01600bd67bc5e67ad15d1347
julien44830 <deniaudjuli0@gmail.com>
2024-06-19 11:13:48 +0200

    feature button disabled if input not valided

--- client/src/pages/parents/ConnexionParent.jsx
+++ client/src/pages/parents/ConnexionParent.jsx
@@ -1,36 +1,48 @@
-/* eslint-disable no-unused-vars */
 import { NavLink } from "react-router-dom";
 import "../../components/styles_components/connexion-parent.css";
 import { useState, useEffect } from "react";
 
 function ConnexionParent() {
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$/;
+  // !explication du regex pour la vérification email
+  //   ^ : Début de la chaîne.
+  // [a-zA-Z0-9._%+-]+ : Un ou plusieurs caractères autorisés dans la partie locale (avant le @). Les caractères autorisés incluent les lettres majuscules et minuscules, les chiffres, ainsi que ._%+-.
+  // @ : Le symbole @ séparant la partie locale et le domaine.
+  // [a-zA-Z0-9.-]+ : Un ou plusieurs caractères autorisés dans le nom de domaine. Les caractères autorisés incluent les lettres majuscules et minuscules, les chiffres, ainsi que .-.
+  // \. : Un point littéral . séparant le domaine et l'extension de domaine.
+  // [a-zA-Z]{2,} : Deux caractères ou plus pour l'extension de domaine (TLD). Les caractères autorisés incluent les lettres majuscules et minuscules.
+  // $ : Fin de la chaîne.
+
+  const regexPassword =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$/;
+  // !explication du regex pour la validation mot de passe
+  //   ^ : Début de la chaîne.
+  // (?=.*[a-z]) : Doit contenir au moins une lettre minuscule.
+  // (?=.*[A-Z]) : Doit contenir au moins une lettre majuscule.
+  // (?=.*\d) : Doit contenir au moins un chiffre.
+  // (?=.*[@$!%*?&]) : Doit contenir au moins un caractère spécial parmi @$!%*?&.
+  // [A-Za-z\d@$!%*?&]{8,} : La chaîne doit être constituée de lettres majuscules, lettres minuscules, chiffres et caractères spéciaux mentionnés, et doit avoir au moins 8 caractères.$ : Fin de la chaîne.
+
+  // declaration des variables d etat pour la validation du formulaire de connection et deblocage du bouton connexion
   const [checkBtnConnexion, setCheckBtnConnexion] = useState(false);
   const [emailChecked, setEmailChecked] = useState(false);
   const [passwordChecked, setPasswordChecked] = useState(false);
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // console.log('%c⧭', 'color: #aa00ff', email);
-  // console.log('%c⧭', 'color: #e50000', password);
-
-  const [data, setData] = useState({
-    email: "",
-    password: "",
-  });
-
-  // console.log('%c⧭', 'color: #00e600', "email",emailChecked);
-  // console.log('%c⧭', 'color: #00a3cc', "password",passwordChecked);
-  // console.log('%c⧭', 'color: #ff0000', "email + password",checkBtnConnexion);
-
+  // recuperation de champ email et verification qu il n a pas d erreure en le comparent au REGEX
   const emailCheck = (e) => {
     setEmail(e.target.value);
+    setEmailChecked(regexEmail.test(email));
   };
 
+  // recuperation de champ password et verification qu il n a pas d erreure en le comparent au REGEX
   const passwordCheck = (e) => {
     setPassword(e.target.value);
+    setPasswordChecked(regexPassword.test(password));
   };
 
+  // deblocage du bouton connexion
   useEffect(() => {
     if (emailChecked && passwordChecked) {
       setCheckBtnConnexion(true);
@@ -96,7 +108,8 @@ function ConnexionParent() {
           </a>
           <NavLink
             to="/accueil"
-            //! className={`link-connexion-parent-btn btn-global texts ${checkBtnConnexion ? "" : "link-connexion-parent-btn-desable"}`}
+            // deblocage du bouton en retirent la className --link-connexion-parent-btn-desable--
+            className={`link-connexion-parent-btn btn-global texts ${checkBtnConnexion ? "" : "link-connexion-parent-btn-desable"}`}
           >
             Se connecter
           </NavLink>
