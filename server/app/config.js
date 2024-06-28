26a0b711d1cfae91002ec8510933fd5f2df22e91
Adeline Cornier <adeline.cornier@hotmail.com>
2024-06-28 12:41:09 +0200

    USY-4_Accueil restore fichier config.js dans dossier server

--- server/app/config.js
+++ server/app/config.js
@@ -14,7 +14,9 @@ const app = express();
 // CORS (Cross-Origin Resource Sharing) is a security mechanism in web browsers that blocks requests from a different domain than the server.
 // You may find the following magic line in forums:
 
-// app.use(cors());
+const cors = require("cors");
+
+app.use(cors());
 
 // You should NOT do that: such code uses the `cors` module to allow all origins, which can pose security issues.
 // For this pedagogical template, the CORS code is commented out to show the need for defining specific allowed origins.
@@ -27,7 +29,6 @@ const app = express();
 // For example: ["http://mysite.com", "http://another-domain.com"]
 
 /*
-const cors = require("cors");
 
 app.use(
   cors({
@@ -55,8 +56,8 @@ app.use(
 
 // Uncomment one or more of these options depending on the format of the data sent by your client:
 
-// app.use(express.json());
-// app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.urlencoded());
 // app.use(express.text());
 // app.use(express.raw());
 
