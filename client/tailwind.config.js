1abd5e56aec70e50cd8c7ce1f0fd2de64b95211d
Lukasz Grzegorzewski <91418482+Lukasz-Grzegorzewski@users.noreply.github.com>
2024-06-28 13:16:15 +0200

    Update tailwind.config.js
    deploy test

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,13 @@
 import { nextui } from "@nextui-org/react";
 
+const importNextUi = process.env.NODE_ENV === "production" ?
+  "../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}" :
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}";
+
 const config = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    importNextUi
   ],
   darkMode: "class",
   theme: {},
