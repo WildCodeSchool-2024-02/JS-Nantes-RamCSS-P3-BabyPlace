506a6d9b0683f6b343adca3e045583013a1a8f5e
julien44830 <deniaudjuli0@gmail.com>
2024-07-22 11:30:18 +0200

    create OptionMenuParent page && add requests.html for use route get nerseries table in local

--- client/src/pages/page_parents/OptionMenuParent.jsx
+++ client/src/pages/page_parents/OptionMenuParent.jsx
@@ -7,14 +7,45 @@ function OptionMenuParent() {
   return (
     <>
       <HeaderReservvationParent />
-      <section className="mt-[200px] flex flex-col">
-        <section className="flex flex-col">
-          <NavLink to="/dossier-parent">Dossier d'inscription</NavLink>
-          <NavLink to="/reservation">Mes réservations</NavLink>
-          <NavLink to="/faq">Aide</NavLink>
+      <section className="mt-[200px] flex flex-col justify-between h-[60vh] texts font-bold">
+        <section className="flex flex-col p-3 gap-5">
+          <NavLink
+            to="/dossier-parent"
+            className="flex gap-2 justify-start items-center"
+          >
+            <img
+              src="../src/assets/images/icônes/icone-dossier.png"
+              alt="dossier d'inscription"
+            />
+            Dossier d'inscription
+          </NavLink>
+          <NavLink
+            to="/reservation"
+            className="flex gap-2 justify-start items-center"
+          >
+            <img
+              src="../src/assets/images/icônes/icone-reservation.png"
+              alt="réservation"
+            />
+            Mes réservations
+          </NavLink>
+          <NavLink to="/faq" className="flex gap-2 justify-start items-center">
+            <img
+              src="../src/assets/images/icônes/icone-aide.png"
+              alt="foire au question"
+            />
+            Aide
+          </NavLink>
         </section>
         <section>
-          <button type="button" className="text-black">
+          <button
+            type="button"
+            className="text-black flex gap-2 justify-start items-center font-bold"
+          >
+            <img
+              src="../src/assets/images/icônes/icone-account-circle.svg"
+              alt=""
+            />
             Déconnexion
           </button>
         </section>
