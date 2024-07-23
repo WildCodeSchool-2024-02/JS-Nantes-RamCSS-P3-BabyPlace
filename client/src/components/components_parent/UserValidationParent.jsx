0b95a9155e7564f2a1c29309a1cfe4500cb86d1a
julien44830 <deniaudjuli0@gmail.com>
2024-07-23 10:42:33 +0200

    dynamic rendering of reservations for parents

--- client/src/components/components_parent/UserValidationParent.jsx
+++ client/src/components/components_parent/UserValidationParent.jsx
@@ -1,26 +1,69 @@
+import { useState, useEffect } from "react";
 import { User } from "@nextui-org/user";
 import { Chip } from "@nextui-org/chip";
 import PropTypes from "prop-types";
 
-function UserValidationParent({ status }) {
+function UserValidationParent({
+  reservationStatus,
+  crecheId,
+  reservationDate,
+}) {
+  const [nurseryInfo, setNurseryInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchCrecheInfo = async () => {
+      try {
+        const url = `${import.meta.env.VITE_API_URL}/api/nurseries/${crecheId}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const nursery = await response.json();
+        setNurseryInfo(nursery);
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
+    };
+
+    fetchCrecheInfo();
+  }, [crecheId]);
+
+  let color = "";
+  let valeur = "";
+
+  if (reservationStatus === 0) {
+    color = "default";
+    valeur = "en cours";
+  } else if (reservationStatus === 1) {
+    color = "success";
+    valeur = "validée";
+  } else if (reservationStatus === 2) {
+    color = "warning";
+    valeur = "attente";
+  } else if (reservationStatus === 3) {
+    color = "danger";
+    valeur = "annulée";
+  }
+
   return (
     <article className="w-[100%] flex justify-between items-center texts">
       <User
-        name="Crèche des Loulous"
-        description="Nom de la crèche"
+        name={nurseryInfo.name}
+        description={nurseryInfo.email}
         avatarProps={{
           src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
         }}
+        reservationName={crecheId}
       />
       <Chip
         className="border rounded-full p-2 text-xs text-white w-[30%]"
-        color={status.color}
+        color={color}
       >
-        {status.valeur}
+        {valeur}
       </Chip>
       <section className="flex flex-col justify-center items-center text-center text-xs  p-5 ">
         <p>vendredi</p>
-        <p>26</p>
+        <p>{reservationDate}</p>
         <p>juillet</p>
       </section>
     </article>
@@ -30,5 +73,7 @@ function UserValidationParent({ status }) {
 export default UserValidationParent;
 
 UserValidationParent.propTypes = {
-  status: PropTypes.string.isRequired,
+  reservationStatus: PropTypes.number.isRequired,
+  crecheId: PropTypes.number.isRequired,
+  reservationDate: PropTypes.string.isRequired,
 };
--- client/src/pages/page_parents/ReservationStatus.jsx
+++ client/src/pages/page_parents/ReservationStatus.jsx
@@ -1,45 +1,23 @@
+import { useLoaderData } from "react-router-dom";
 import Toolbar from "../../components/components_parent/Toolbar";
 import HeaderReservvationParent from "../../components/components_parent/HeaderReservationParent";
 import UserValidationParent from "../../components/components_parent/UserValidationParent";
 
-const status = [
-  {
-    color: "default",
-    valeur: "en cours",
-  },
-  {
-    color: "success",
-    valeur: "validée",
-  },
-  {
-    color: "warning",
-    valeur: "attente",
-  },
-  {
-    color: "danger",
-    valeur: "annulée",
-  },
-];
-
 function ReservationStatus() {
+  const reservation = useLoaderData();
+
   return (
     <>
       <HeaderReservvationParent />
       <section className="p-6 w-[100vw] flex flex-col justify-center itens-center mt-48">
-        <UserValidationParent status={status[0]} />
-        <UserValidationParent status={status[3]} />
-        <UserValidationParent status={status[2]} />
-        <UserValidationParent status={status[0]} />
-        <UserValidationParent status={status[1]} />
-        <UserValidationParent status={status[3]} />
-        <UserValidationParent status={status[1]} />
-        <UserValidationParent status={status[1]} />
-        <UserValidationParent status={status[1]} />
-        <UserValidationParent status={status[1]} />
-        <UserValidationParent status={status[1]} />
-        <UserValidationParent status={status[1]} />
-        <UserValidationParent status={status[1]} />
-        <UserValidationParent status={status[1]} />
+        {reservation.map((e) => (
+          <UserValidationParent
+            key={e}
+            crecheId={e.nursery_id}
+            reservationDate={e.reservation_date}
+            reservationStatus={e.reservation_status}
+          />
+        ))}
       </section>
       <Toolbar />
     </>
