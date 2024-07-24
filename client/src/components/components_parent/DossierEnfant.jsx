f4505daaef2733890f79787c1ed6a3333607ecd9
julien44830 <deniaudjulien0@gmail.com>
2024-07-25 01:36:51 +0200

    functional child file component

--- client/src/components/components_parent/DossierEnfant.jsx
+++ client/src/components/components_parent/DossierEnfant.jsx
@@ -1,166 +1,3 @@
-// import { useState } from "react";
-// import { Input } from "@nextui-org/input";
-// import { Checkbox } from "@nextui-org/checkbox";
-// import { DatePicker } from "@nextui-org/date-picker";
-
-// function DossierEnfant() {
-//   const userId = localStorage.getItem("parent_id");
-//   const [formData, setFormData] = useState({
-//     firstname: "",
-//     lastname: "",
-//     birthday: "2022-04-19",
-//     is_walker: false,
-//     is_disabled: false,
-//     allergies: false,
-//     health_book: false,
-//     name_doctor: "",
-//     care_authorization: false,
-//     parent_id: userId,
-//     insurance_certificate: false,
-//   });
-//   // console.log('%c⧭', 'color: #917399', formData);
-
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData((prevFormData) => ({
-//       ...prevFormData,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleCheckboxChange = (e) => {
-//     const { name, checked } = e.target;
-//     setFormData({ ...formData, [name]: checked });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     if (!userId) {
-//       console.error("ID utilisateur introuvable");
-//       return;
-//     }
-
-//     // Appel API pour envoyer les données à la base de données
-//     try {
-//       const response = await fetch(
-//         `${import.meta.env.VITE_API_URL}/api/children/`,
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify(formData),
-//         }
-//       );
-
-//       if (!response.ok) {
-//         throw new Error("Erreur lors de l'envoi des données");
-//       }
-//     } catch (error) {
-//       console.error("Erreur:", error);
-//       // Gérez les erreurs ici
-//     }
-//   };
-
-//   return (
-//     <>
-//     <select name="children.id" id="">
-//       <option value="1">nouvelle enfant</option>
-//     </select>
-//       <form className="h-[72vh] bg-white flex flex-center flex-col items-center justify-center gap-2">
-//         <Input
-//           isRequired
-//           type="text"
-//           label="Prénom"
-//           className="max-w-[284px] texts"
-//           size="lg"
-//           name="firstname"
-//           onChange={handleInputChange}
-//         />
-//         <Input
-//           isRequired
-//           type="text"
-//           label="Nom"
-//           className="max-w-[284px] texts"
-//           size="lg"
-//           name="lastname"
-//           onChange={handleInputChange}
-//         />
-
-//         <DatePicker
-//           label="Birth date"
-//           className="max-w-[284px]"
-//           // selected={formData.birthdate}
-//           onChange={(date) => setFormData({ ...formData, birthday: date })}
-//         />
-
-//         <section className="flex flex-col items-start justify-start gap-5 w-[280px]">
-//           <Checkbox
-//             color="secondary"
-//             name="is_walker"
-//             checked={formData.is_walker}
-//             onChange={handleCheckboxChange}
-//           >
-//             Marche-t-il ?
-//           </Checkbox>
-//           <Checkbox
-//             color="secondary"
-//             name="is_disabled"
-//             checked={formData.is_disabled}
-//             onChange={handleCheckboxChange}
-//           >
-//             A-t-il un handicap reconnu ?
-//           </Checkbox>
-//           <Checkbox
-//             color="secondary"
-//             name="allergies"
-//             checked={formData.allergies}
-//             onChange={handleCheckboxChange}
-//           >
-//             A-t-il des allergies ?
-//           </Checkbox>
-//           <Checkbox
-//             color="secondary"
-//             name="health_book"
-//             checked={formData.health_book}
-//             onChange={handleCheckboxChange}
-//           >
-//             Carnet de santé ?
-//           </Checkbox>
-//           <Checkbox
-//             color="secondary"
-//             name="care_authorization"
-//             checked={formData.care_authorization}
-//             onChange={handleCheckboxChange}
-//           >
-//             autorisation de soins ?
-//           </Checkbox>
-//         </section>
-//         <Input
-//           type="text"
-//           className="max-w-[284px] texts"
-//           size="lg"
-//           placeholder="Nom du medecin traitant"
-//           name="name_doctor"
-//           value={formData.name_doctor}
-//           onChange={handleInputChange}
-//         />
-
-//         <button
-//           type="submit"
-//           onClick={handleSubmit}
-//           className="bg-gradient-to-tr from-purple-600 to-blue-400 text-white shadow-lg texts"
-//         >
-//           Enregistrer
-//         </button>
-//       </form>
-//     </>
-//   );
-// }
-
-// export default DossierEnfant;
-
 import { useState, useEffect } from "react";
 import { Input } from "@nextui-org/input";
 import { Checkbox } from "@nextui-org/checkbox";
@@ -173,7 +10,7 @@ function DossierEnfant() {
   const [formData, setFormData] = useState({
     firstname: "",
     lastname: "",
-    birthday: "2022-04-19",
+    birthday: "",
     is_walker: false,
     is_disabled: false,
     allergies: false,
@@ -217,7 +54,7 @@ function DossierEnfant() {
       setFormData({
         firstname: "",
         lastname: "",
-        birthday: "2022-04-19",
+        birthday: "",
         is_walker: false,
         is_disabled: false,
         allergies: false,
@@ -254,24 +91,24 @@ function DossierEnfant() {
   };
 
   const handleDateChange = (date) => {
+    // Créer une date en utilisant les propriétés fournies par le DatePicker
+    const { year, month, day } = date;
+    const newDate = new Date(year, month - 1, day + 1);
     setFormData((prevFormData) => ({
       ...prevFormData,
-      birthday: date.toISOString().split("T")[0], // Format yyyy-mm-dd
+      birthday: newDate.toISOString().split("T")[0], // Format yyyy-mm-dd
     }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (!userId) {
       console.error("ID utilisateur introuvable");
       return;
     }
-
     try {
       const method = selectedChildId === "new" ? "POST" : "PUT";
       const url = `${import.meta.env.VITE_API_URL}/api/children${selectedChildId !== "new" ? `/${selectedChildId}` : ""}`;
-
       const response = await fetch(url, {
         method,
         headers: {
@@ -279,21 +116,16 @@ function DossierEnfant() {
         },
         body: JSON.stringify(formData),
       });
-
       if (!response.ok) {
         throw new Error("Erreur lors de l'envoi des données");
       }
-
       const result = await response.json();
-
       // Mettre à jour la liste des enfants après une soumission réussie
       const updatedChildren =
         selectedChildId === "new"
           ? [...children, result]
           : children.map((child) => (child.id === result.id ? result : child));
-
       setChildren(updatedChildren);
-
       if (selectedChildId === "new") {
         setSelectedChildId(result.id.toString());
       }
@@ -303,7 +135,10 @@ function DossierEnfant() {
   };
 
   return (
-    <>
+    <form
+      onSubmit={handleSubmit}
+      className="h-[72vh] bg-white flex flex-center flex-col items-center justify-center gap-2"
+    >
       <select
         name="children"
         onChange={handleSelectChange}
@@ -320,95 +155,91 @@ function DossierEnfant() {
           <option disabled>Pas d'enfants disponibles</option>
         )}
       </select>
-      <form
-        onSubmit={handleSubmit}
-        className="h-[72vh] bg-white flex flex-center flex-col items-center justify-center gap-2"
-      >
-        <Input
-          isRequired
-          type="text"
-          label="Prénom"
-          className="max-w-[284px] texts"
-          size="lg"
-          name="firstname"
-          value={formData.firstname}
-          onChange={handleInputChange}
-        />
-        <Input
-          isRequired
-          type="text"
-          label="Nom"
-          className="max-w-[284px] texts"
-          size="lg"
-          name="lastname"
-          value={formData.lastname}
-          onChange={handleInputChange}
-        />
-        <DatePicker
-          label="Date de naissance"
-          className="max-w-[284px]"
-          selected={new Date(formData.birthday)}
-          onChange={handleDateChange}
-        />
-        <section className="flex flex-col items-start justify-start gap-5 w-[280px]">
-          <Checkbox
-            color="secondary"
-            name="is_walker"
-            checked={formData.is_walker}
-            onChange={handleCheckboxChange}
-          >
-            Marche-t-il ?
-          </Checkbox>
-          <Checkbox
-            color="secondary"
-            name="is_disabled"
-            checked={formData.is_disabled}
-            onChange={handleCheckboxChange}
-          >
-            A-t-il un handicap reconnu ?
-          </Checkbox>
-          <Checkbox
-            color="secondary"
-            name="allergies"
-            checked={formData.allergies}
-            onChange={handleCheckboxChange}
-          >
-            A-t-il des allergies ?
-          </Checkbox>
-          <Checkbox
-            color="secondary"
-            name="health_book"
-            checked={formData.health_book}
-            onChange={handleCheckboxChange}
-          >
-            Carnet de santé ?
-          </Checkbox>
-          <Checkbox
-            color="secondary"
-            name="care_authorization"
-            checked={formData.care_authorization}
-            onChange={handleCheckboxChange}
-          >
-            Autorisation de soins ?
-          </Checkbox>
-        </section>
-        <Input
-          type="text"
-          className="max-w-[284px] texts"
-          size="lg"
-          placeholder="Nom du médecin traitant"
-          name="name_doctor"
-          value={formData.name_doctor}
-          onChange={handleInputChange}
-        />
-        <button
-          type="submit"
-          className="bg-gradient-to-tr from-purple-600 to-blue-400 text-white shadow-lg texts"
+      <Input
+        isRequired
+        type="text"
+        label="Prénom"
+        className="max-w-[284px] texts"
+        size="lg"
+        name="firstname"
+        value={formData.firstname}
+        onChange={handleInputChange}
+      />
+      <Input
+        isRequired
+        type="text"
+        label="Nom"
+        className="max-w-[284px] texts"
+        size="lg"
+        name="lastname"
+        value={formData.lastname}
+        onChange={handleInputChange}
+      />
+      <DatePicker
+        name="birthday"
+        label="Date de naissance"
+        className="max-w-[284px]"
+        selected={formData.birthday}
+        onChange={handleDateChange}
+      />
+      <section className="flex flex-col items-start justify-start gap-5 w-[280px]">
+        <Checkbox
+          color="secondary"
+          name="is_walker"
+          checked={formData.is_walker}
+          onChange={handleCheckboxChange}
         >
-          Enregistrer
-        </button>
-      </form>
-    </>
+          Marche-t-il ?
+        </Checkbox>
+        <Checkbox
+          color="secondary"
+          name="is_disabled"
+          checked={formData.is_disabled}
+          onChange={handleCheckboxChange}
+        >
+          A-t-il un handicap reconnu ?
+        </Checkbox>
+        <Checkbox
+          color="secondary"
+          name="allergies"
+          checked={formData.allergies}
+          onChange={handleCheckboxChange}
+        >
+          A-t-il des allergies ?
+        </Checkbox>
+        <Checkbox
+          color="secondary"
+          name="health_book"
+          checked={formData.health_book}
+          onChange={handleCheckboxChange}
+        >
+          Carnet de santé ?
+        </Checkbox>
+        <Checkbox
+          color="secondary"
+          name="care_authorization"
+          checked={formData.care_authorization}
+          onChange={handleCheckboxChange}
+        >
+          Autorisation de soins ?
+        </Checkbox>
+      </section>
+      <Input
+        type="text"
+        className="max-w-[284px] texts"
+        size="lg"
+        placeholder="Nom du médecin traitant"
+        name="name_doctor"
+        value={formData.name_doctor}
+        onChange={handleInputChange}
+      />
+      <button
+        type="submit"
+        className="bg-gradient-to-tr from-purple-600 to-blue-400 text-white shadow-lg texts"
+      >
+        Enregistrer
+      </button>
+    </form>
   );
 }
 
